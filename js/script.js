3b145ce20169cfa212c512e78d0607f88b6e1efc
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-04-09 13:15:39 +0200

    chiamo l'api con mounted e con un metodo e stampo in console

--- js/script.js
+++ js/script.js
@@ -1,16 +1,43 @@
+//Api Key: febf9a4fc7b46ba8b5e4681cf81209ce
+
 var root = new Vue({
 el: '#root',
  data: {
+   movies: [],
+   search: "",
 
 
 
     },
    mounted: function() {
-
+     axios.get('https://api.themoviedb.org/3/search/movie' , {
+       params: {
+         api_key: "febf9a4fc7b46ba8b5e4681cf81209ce",
+         query:"fight",
+         language: "it-IT"
+       }
+     })
+     .then( function (response) {
+       this.movies = response.data.results;
+       console.log(this.movies);
+     });
 
    },
 
    methods: {
+     searchFromApi: function() {
+       axios.get('https://api.themoviedb.org/3/search/movie' , {
+         params: {
+           api_key: "febf9a4fc7b46ba8b5e4681cf81209ce",
+           query:"fight",
+           language: "it-IT"
+         }
+       })
+       .then( (response) => {
+         this.movies = response.data.results;
+         console.log(this.movies);
+       });
+     }
 
 
    }
