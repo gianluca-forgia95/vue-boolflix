41a1696186a02f73f8d827a2691d26d32ded24d3
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-04-12 17:39:07 +0200

    inserite serie-tv

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
 //Api Key: febf9a4fc7b46ba8b5e4681cf81209ce
+//Url Base: https://api.themoviedb.org/3/
 
 var root = new Vue({
 el: '#root',
  data: {
    movies: [],
+   series: [],
    search: "",
+   apiKey: 'febf9a4fc7b46ba8b5e4681cf81209ce',
+   urlSearch: 'https://api.themoviedb.org/3/search/',
 
     },
+
    mounted: function() {
 
    },
 
    methods: {
      searchFromApi: function() {
-       axios.get('https://api.themoviedb.org/3/search/movie' , {
+       //Chiamata per i Film
+       axios.get( this.urlSearch + 'movie' , {
          params: {
-           api_key: "febf9a4fc7b46ba8b5e4681cf81209ce",
+           api_key: this.apiKey,
            query: this.search,
            language: "it-IT"
          }
@@ -24,7 +30,22 @@ el: '#root',
          this.movies = response.data.results;
          this.search = "";
          for (var i = 0; i < this.movies.length; i++) {
-            this.movies[i].vote_average = Math.ceil(this.movies[i].vote_average / 2)
+            this.movies[i].vote_average = Math.ceil(this.movies[i].vote_average / 2);
+         }
+       });
+       //Chiamata per le SerieTV
+       axios.get( this.urlSearch + 'tv' , {
+         params: {
+           api_key: this.apiKey,
+           query: this.search,
+           language: "it-IT"
+         }
+       })
+       .then( (response) => {
+         this.series = response.data.results;
+         this.search = "";
+         for (var i = 0; i < this.series.length; i++) {
+            this.series[i].vote_average = Math.ceil(this.series[i].vote_average / 2);
          }
        });
 
